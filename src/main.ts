ed8c36ebc2a605424e12d02115f2cacb40171f76
sunboy-dev <slapexs@gmail.com>
2025-09-28 19:19:58 +0700

    feat: add notes module with CRUD operations and Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle("Remember Me API Documentation")
+    .setDescription("API documentation for the Remember Me application")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup("swagger", app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
